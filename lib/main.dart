472c8f2b340079f695c1abd39a680cf81bcf7fd4
swansea <leesuwan89@gmail.com>
2024-12-24 07:24:03 +0900

    헤더 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,44 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Hello flutter"),
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40), // x축 패딩
+          child: Column(
+            // 위에 쌓기 위해 Column을 쓴다.
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                // 수평 배치
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "이렇게 그지같이 작성해?",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "어쨌든 웰컴",
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
           ),
-          body: Center(
-            child: Text("헬로 월드"),
-          )),
-    );
+        ),
+      ), // Scaffold 닫는 괄호
+    ); // MaterialApp 닫는 괄호
   }
 }
